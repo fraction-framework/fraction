c3e5bd27d2f6b3d0212d75c6004a006d60e8a27c
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2023-08-01 01:28:51 +0300

    Refactor Event creation
    Make it simpler and maintainable

--- src/Component/Event/Enum/EventType.php
+++ src/Component/Event/Enum/EventType.php
@@ -2,13 +2,20 @@
 
 namespace Fraction\Component\Event\Enum;
 
+use Fraction\Component\Event\EventTypes\ControllerEvent;
+use Fraction\Component\Event\EventTypes\Event;
+use Fraction\Component\Event\EventTypes\ExceptionEvent;
+use Fraction\Component\Event\EventTypes\RequestEvent;
+use Fraction\Component\Event\EventTypes\ResponseEvent;
+use Fraction\Component\Event\EventTypes\ViewEvent;
+
 enum EventType: string {
-  case Request = 'request';
-  case Response = 'response';
-  case Controller = 'controller';
-  case View = 'view';
-  case Exception = 'exception';
-  case Terminate = 'terminate';
+  case Request = RequestEvent::class;
+  case Response = ResponseEvent::class;
+  case Controller = ControllerEvent::class;
+  case View = ViewEvent::class;
+  case Exception = ExceptionEvent::class;
+  case Terminate = Event::class;
 
   public function getValue(): string {
     return $this->value;
